8d88acb29f4a1292a49f676a3e9296d563180f35
Holden <htfenner@onondaga.io>
2020-11-06 23:14:25 -0500

    Environment scripts :

---
+++ infrabuild.sh
@@ -0,0 +1,12 @@
+export VMWARE_USER=administrator@vsphere.infrabuild.xyz
+export VMWARE_USERNAME=${VMWARE_USER}
+export VSPHERE_USER=${VMWARE_USER}
+export VMWARE_PASSWORD=BadPassword1234$
+export VSPHERE_PASSWORD=${VMWARE_USER}
+export VMWARE_HOST=vsphere.infrabuild.xyz
+export VMWARE_SERVER=${VMWARE_HOST}
+export VSPHERE_SERVER=${VMWARE_HOST}
+export VSPHERE_ALLOW_UNVERIFIED_SSL=true
+export GOVC_USERNAME=${VMWARE_USER}
+export GOVC_PASSWORD=${VMWARE_PASSWORD}
+export GOVC_URL=${VMWARE_HOST}
---
+++ setup.sh
@@ -0,0 +1,3 @@
+export PACKER_CACHE_DIR=/build_artifact
+export ANSIBLE_ROLES_PATH=./roles/:../roles:~/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles
+
